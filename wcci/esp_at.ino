963c019a3ee8f8ecaee181721bef55e769705bfa
Johan Sebring <johan.sebring@gmail.com>
2016-01-05 13:50:42 +0100

    Tidy code

--- wcci/esp_at.ino
+++ wcci/esp_at.ino
@@ -3,160 +3,104 @@
 bool sendATCipClose() {
   bool res = false;
 
-
   Serial.println("AT+CIPCLOSE");
   // listen for response
-    if (Serial.find("OK")) {
-
-      res = true;
-
-    } else {
-
-    }
-  
+  if (Serial.find("OK")) {
+    res = true;
+  } else { }
   return res;
 }
 
 bool sendATUnlockDoor() {
     bool res = false;
-    String url_path = "GET /log_visit_state/0 HTTP/1.0\r\n\r\n";
+    String url_path = "GET /log_visit_state/0 HTTP/1.1\r\n\r\n";
 
   Serial.println(url_path);
   // listen for response
     if (Serial.find("OK")) {
-
       res = true;
-
-    } else {
-
-    }
-
-  
+    } else { }
   return res;
 }
 
 
 bool sendATPrepareUnlockDoor() {
-    bool res = false;
-    String url_path = "GET /log_visit_state/0 HTTP/1.0\r\n\r\n";
+  bool res = false;
+  String url_path = "GET /log_visit_state/0 HTTP/1.1\r\n\r\n";
 
   Serial.print("AT+CIPSEND=4,");
   Serial.println(url_path.length());
   // listen for response
-    if (Serial.find(">")) {
-
-      res = true;
-
-    } else {
-
-    }
-  
-  
+  if (Serial.find(">")) {
+    res = true;
+  } else { }
   return res;
 }
-
 
 bool sendATLockDoor() {
     bool res = false;
-    String url_path = "GET /log_visit_state/1 HTTP/1.0\r\n\r\n";
+    String url_path = "GET /log_visit_state/1 HTTP/1.1\r\n\r\n";
 
   Serial.println(url_path);
   // listen for response
-    if (Serial.find("OK")) {
-
-      res = true;
-
-    } else {
-
-    }
-
-  
+  if (Serial.find("OK")) {
+    res = true;
+    } else { }
   return res;
 }
 
-
 bool sendATPrepareLockDoor() {
-    bool res = false;
-    String url_path = "GET /log_visit_state/1 HTTP/1.0\r\n\r\n";
+  bool res = false;
+  String url_path = "GET /log_visit_state/1 HTTP/1.1\r\n\r\n";
 
   Serial.print("AT+CIPSEND=4,");
   Serial.println(url_path.length());
   // listen for response
-    if (Serial.find(">")) {
-
-      res = true;
-
-    } else {
-
-    }
-
-  
+  if (Serial.find(">")) {
+    res = true;
+  } else { }
   return res;
 }
 
 bool sendATConnectServer() {
   bool res = false;
 
-  Serial.println("AT+CIPSTART=4,\"TCP\",\"10.225.56.58\",3000");
+  Serial.println("AT+CIPSTART=4,\"TCP\",\"10.225.56.65\",3000");
   // listen for response
-    if (Serial.find("OK")) {
-
-      res = true;
-
-    } else {
-
-    }
-
-  
+  if (Serial.find("OK")) {
+    res = true;
+  } else { }
   return res;
 }
 
 bool sendATMultipleConnections() {
-  
   bool res = false;
-  
   Serial.println("AT+CIPMUX=1");
-  
-
   // listen for response
-    if (Serial.find("OK")) {
-
-      res = true;
-
-    } else {
-
-    }
+  if (Serial.find("OK")) {
+    res = true;
+  } else { }
   return res;
 }
 
 bool sendATJoinNetwork() {
   bool res = false;
-//  String SSID = "";
+  //  String SSID = "";
   Serial.println("AT+CWJAP=" + SSID + "," + PWD );
   // listen for response
-    if (Serial.find("OK")) {
-
-      res = true;
-
-    } else {
-
-    }
-  
+  if (Serial.find("OK")) {
+    res = true;
+  } else { }
   return res;
 }
 
 bool sendATModeStation() {
   bool res = false;
-  Serial.println("AT+CWMODE=3");
-  
-
+  Serial.println("AT+CWMODE=1");
   // listen for response
-    if (Serial.find("OK")) {
-
-      res = true;
-
-    }
-
+  if (Serial.find("OK")) {
+    res = true;
+  } else {}
   return res;
 }
 
@@ -165,11 +109,11 @@ bool sendATReset() {
   Serial.println("AT+RST");
   
   // listen for response
-    if (Serial.find("ready")) {
-      res = true;
-    } 
-    delay(500);
-    return res;
+  if (Serial.find("ready")) {
+    res = true;
+  } 
+  delay(500);
+  return res;
 }
 
 
@@ -181,23 +125,3 @@ bool sendAT() {
   return false;
 }
 
-/*
-void blink() {
-        digitalWrite(GREEN, HIGH);
-        digitalWrite(YELLOW, HIGH);
-        digitalWrite(RED, HIGH);
-        delay(500);
-        digitalWrite(GREEN, LOW);
-        digitalWrite(YELLOW, LOW);
-        digitalWrite(RED, LOW);
-        delay(500);
-        digitalWrite(GREEN, HIGH);
-        digitalWrite(YELLOW, HIGH);
-        digitalWrite(RED, HIGH);
-        delay(500);
-        digitalWrite(GREEN, LOW);
-        digitalWrite(YELLOW, LOW);
-        digitalWrite(RED, LOW);
-        delay(500);
-}
-*/
